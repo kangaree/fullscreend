5b46b1b504f7bf741394b9cbc1f8bd0a68099a32
Gary Kang <garyzkang@gmail.com>
2019-01-22 14:56:41 -0500

    Start Rails User/Session REST

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
 end

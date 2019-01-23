92b45692998f821fa2c4b9e36696e0031d44f71b
Gary Kang <garyzkang@gmail.com>
2019-01-23 01:39:57 -0500

    Make Basic CSS

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 export default () => (
     <div className="home">
-        <h2>Social TV Discovery.</h2>
-        <img src="https://image.tmdb.org/t/p/original/eSzpy96DwBujGFj0xMbXBcGcfxX.jpg" alt=""/>
+        <h2>The social network for tv lovers.</h2>
+        <div class="list"> 
+            <img src="https://image.tmdb.org/t/p/original/wjFU9z8EfOwczRTv0FrPcv9zHME.jpg" alt="Cowboy Bebop"/>
+            <img src="https://image.tmdb.org/t/p/original/dg7NuKDjmS6OzuNy33qt8kSkPA1.jpg" alt="The Wire"/>
+            <img src="https://image.tmdb.org/t/p/original/vNJ95BMCzIBJZhW0IEoxzOoW53P.jpg" alt="Stranger Things"/>
+            <img src="https://image.tmdb.org/t/p/original/iE3s0lG5QVdEHOEZnoAxjmMtvne.jpg" alt="One Punch Man"/>
+            <img src="https://image.tmdb.org/t/p/original/tPqV63zcW6ZV0Hd48DMGb5UzQIG.jpg" alt="Freaks and Geeks"/>
+            <img src="https://image.tmdb.org/t/p/original/aCw8ONfyz3AhngVQa1E2Ss4KSUQ.jpg" alt="Seinfeld"/>
+        </div>
     </div>
 )
--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -27,7 +27,7 @@ class Signup extends React.Component {
     render() {
         return (
             <div className="session-form">
-                <h2>Sign Up!!!!!!</h2>
+                <h2>Sign Up</h2>
                 <form>
                     <label>Username:
                         <input

3731bd9c3fbce1604bb8988e4e77073c2a01b50d
Gary Kang <garyzkang@gmail.com>
2019-01-23 19:34:35 -0500

    Complete Nav Bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -8,15 +8,24 @@ export default ({ currentUser, logout }) => {
       <button onClick={logout}>Log Out</button>
     </div>
   ) : (
-      <div>
-        <Link className="btn" to="/signup">Sign Up</Link>
-        <Link className="btn" to="/login">Log In</Link>
-      </div>
+      <nav className="header-nav">
+        <ul>
+          <li><Link className="nav-link" to="/login">Sign In</Link></li>
+          <li><Link className="nav-link" to="/signup">Create Account</Link></li>
+          <li><Link className="nav-link" to="/shows">Shows</Link></li>
+          <li><Link className="nav-link" to="/lists">Lists</Link></li>
+          <li><Link className="nav-link" to="/users">People</Link></li >
+        </ul>
+          <input type="text" />
+      </nav>
     );
 
   return (
     <header className="nav-bar">
-      <Link to="/"><h1 className="logo">Fullscreend</h1></Link>
+      <div className="nav-logo-text">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/%D0%A3%D0%AD%D0%98%D0%A2.svg" />
+        <Link to="/"><h1 className="logo">Fullscreend</h1></Link>
+      </div>
       <div>
         {display}
       </div>

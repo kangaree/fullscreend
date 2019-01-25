b7bd7b2db6566095f35c1cff81e77781c2de6910
Gary Kang <garyzkang@gmail.com>
2019-01-25 11:48:30 -0500

    Make Demo User Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+demo_user = User.create(email:'test@12345fakestreet.com', username: 'test_user', password: 'test_user')

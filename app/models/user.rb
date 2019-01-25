398a62aadbc26913d85d0b4247eac6e31922bccf
Gary Kang <garyzkang@gmail.com>
2019-01-25 10:14:22 -0500

    Add username, email, and session_token uniqueness model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  validates :username, :session_token, :email, uniqueness: true
 
   attr_reader :password
 

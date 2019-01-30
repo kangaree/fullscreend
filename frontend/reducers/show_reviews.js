5e06b1ba1c880866ef7ec56a576fd93ef1d69d4c
Gary Kang <garyzkang@gmail.com>
2019-01-30 16:36:34 -0500

    showReviewsReducer takes in a new id when a review is received

--- frontend/reducers/show_reviews.js
+++ frontend/reducers/show_reviews.js
@@ -1,15 +1,19 @@
 import merge from 'lodash/merge';
 
 import {
-    RECEIVE_SHOW_REVIEWS
+    RECEIVE_SHOW_REVIEWS, RECEIVE_REVIEW
 } from '../actions/show_actions';
 
 const showReviewsReducer = (state = [], action) => {
     Object.freeze(state)
-    // debugger
     switch (action.type) {
+        case RECEIVE_REVIEW:
+            let newState = merge([], state);
+            newState.push(action.review.id);
+            return newState;
+            // return merge([], state, [action.review.id])
         case RECEIVE_SHOW_REVIEWS:
-            return action.reviews.reviews ? Object.keys(action.reviews.reviews).map(num => parseInt(num)) : [];
+            return action.reviews.reviews ? merge([],state,Object.keys(action.reviews.reviews).map(num => parseInt(num))) : state;
         default:
             return state;
     }

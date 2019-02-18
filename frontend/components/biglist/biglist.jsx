e934ab9aa24b366d031e3a4f5097d767e9be7c5f
Gary Kang <garyzkang@gmail.com>
2019-02-17 20:54:02 -0500

    Add Character Titles to Big List

--- frontend/components/biglist/biglist.jsx
+++ frontend/components/biglist/biglist.jsx
@@ -6,41 +6,41 @@ export default () => (
         <div className="blank-header"></div>
         <div className="show-index">
             <ul className="shows-ul-top">
-                <li className="show-li-top"><Link to="/shows/58957" className=""><img src="https://i.imgur.com/f78tmMy.png" alt="Nathan For You" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/615" className=""><img src="https://i.imgur.com/9GNktMV.png" alt="Futurama" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/387" className=""><img src="https://i.imgur.com/VIMwbM4.png" alt="SpongeBob SquarePants" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/60572" className=""><img src="https://i.imgur.com/ixlw0og.png" alt="Pokémon" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/4385" className=""><img src="https://i.imgur.com/YbkjZ7v.png" alt="The Colbert Report" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/60625" className=""><img src="https://i.imgur.com/ycqqdBC.png" alt="Rick and Morty" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/4946" className=""><img src="https://i.imgur.com/4X0lAzh.png" alt="Deal or No Deal" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/2190" className=""><img src="https://i.imgur.com/KOXli1s.png" alt="South Park" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/1104" className=""><img src="https://i.imgur.com/Pjnm3OT.png" alt="Mad Men" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/17287" className=""><img src="https://i.imgur.com/VyPEyzZ.png" alt="Party Down" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/66573" className=""><img src="https://i.imgur.com/R2OaXJK.png" alt="The Good Place" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/32726" className=""><img src="https://i.imgur.com/5t7WwU9.png" alt="Bob's Burgers" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/30991" className=""><img src="https://i.imgur.com/RL7UjPl.png" alt="Cowboy Bebop" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/2673" className=""><img src="https://i.imgur.com/Of2Yclr.png" alt="The O.C." /></Link></li>
-                <li className="show-li-top"><Link to="/shows/7074" className=""><img src="https://i.imgur.com/DwoyXgm.png" alt="America's Best Dance Crew" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/60728" className=""><img src="https://i.imgur.com/NQWpg1t.png" alt="Kill La Kill" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/4607" className=""><img src="https://i.imgur.com/GPOb988.png" alt="Lost" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/74577" className=""><img src="https://i.imgur.com/vFYvQvo.png" alt="The End of the F***ing World" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/2025" className=""><img src="https://i.imgur.com/RC4E44c.png" alt="Beverly Hills, 90210" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/45" className=""><img src="https://i.imgur.com/ASiZ5dR.png" alt="Top Gear" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/4589" className=""><img src="https://i.imgur.com/GGFeXP6.png" alt="Arrested Development" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/12143" className=""><img src="https://i.imgur.com/P6O2mqS.png" alt="Azumanga Daioh" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/2423" className=""><img src="https://i.imgur.com/HQAUUX9.png" alt="Celebrity Deathmatch" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/4313" className=""><img src="https://i.imgur.com/81obnYi.png" alt="Full House" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/1430" className=""><img src="https://i.imgur.com/uAdlbyf.png" alt="Cosmos" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/456" className=""><img src="https://i.imgur.com/Z0J4JVS.png" alt="The Simpsons" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/52" className=""><img src="https://i.imgur.com/fylNpug.png" alt="That '70s Show" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/63161" className=""><img src="https://i.imgur.com/HM0JYAF.png" alt="Crazy Ex-Girlfriend" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/61222" className=""><img src="https://i.imgur.com/0uB0ib2.png" alt="Bojack Horseman" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/251" className=""><img src="https://i.imgur.com/LIorqqJ.png" alt="Aqua Teen Hunger Force" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/60839" className=""><img src="https://i.imgur.com/aE6SYNl.png" alt="Broad City" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/62560" className=""><img src="https://i.imgur.com/im2kayJ.png" alt="Mr. Robot" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/2316" className=""><img src="https://i.imgur.com/JmqJCiW.png" alt="The Office" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/12971" className=""><img src="https://i.imgur.com/4Tx8mGx.png" alt="Dragon Ball Z" /></Link></li>
-                <li className="show-li-top"><Link to="/shows/60573" className=""><img src="https://i.imgur.com/djMlQI5.png" alt="Silicon Valley" /></Link></li>
+                <li className="show-li-top"><Link to="/shows/58957" className=""><img src="https://i.imgur.com/f78tmMy.png" alt="Nathan For You" title="Nathan Fielder"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/615" className=""><img src="https://i.imgur.com/9GNktMV.png" alt="Futurama" title="Bender Rodriguez"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/387" className=""><img src="https://i.imgur.com/VIMwbM4.png" alt="SpongeBob SquarePants" title="Gary"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/60572" className=""><img src="https://i.imgur.com/ixlw0og.png" alt="Pokémon" title="Pikachu"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/4385" className=""><img src="https://i.imgur.com/YbkjZ7v.png" alt="The Colbert Report" title="Stephen Colbert"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/60625" className=""><img src="https://i.imgur.com/ycqqdBC.png" alt="Rick and Morty" title="Rick and Morty"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/4946" className=""><img src="https://i.imgur.com/4X0lAzh.png" alt="Deal or No Deal" title="Howie Mandel"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/2190" className=""><img src="https://i.imgur.com/KOXli1s.png" alt="South Park" title="The Boys"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/1104" className=""><img src="https://i.imgur.com/Pjnm3OT.png" alt="Mad Men" title="Sterling Cooper Advertising Agency"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/17287" className=""><img src="https://i.imgur.com/VyPEyzZ.png" alt="Party Down" title="Party Down Catering"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/66573" className=""><img src="https://i.imgur.com/R2OaXJK.png" alt="The Good Place" title="Good Place"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/32726" className=""><img src="https://i.imgur.com/5t7WwU9.png" alt="Bob's Burgers" title="Belcher Family"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/30991" className=""><img src="https://i.imgur.com/RL7UjPl.png" alt="Cowboy Bebop" title="Bebop Crew"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/2673" className=""><img src="https://i.imgur.com/Of2Yclr.png" alt="The O.C." title="Orange County"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/7074" className=""><img src="https://i.imgur.com/DwoyXgm.png" alt="America's Best Dance Crew" title="Jabbawockeez"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/60728" className=""><img src="https://i.imgur.com/NQWpg1t.png" alt="Kill La Kill" title="Ryuko Matoi"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/4607" className=""><img src="https://i.imgur.com/GPOb988.png" alt="Lost" title="Hurley"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/74577" className=""><img src="https://i.imgur.com/vFYvQvo.png" alt="The End of the F***ing World" title="James and Alyssa"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/2025" className=""><img src="https://i.imgur.com/RC4E44c.png" alt="Beverly Hills, 90210" title="90210"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/45" className=""><img src="https://i.imgur.com/ASiZ5dR.png" alt="Top Gear" title="Jeremy, Richard, James"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/4589" className=""><img src="https://i.imgur.com/GGFeXP6.png" alt="Arrested Development" title="Bluth Family"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/12143" className=""><img src="https://i.imgur.com/P6O2mqS.png" alt="Azumanga Daioh" title="Azumanga Daioh"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/2423" className=""><img src="https://i.imgur.com/HQAUUX9.png" alt="Celebrity Deathmatch" title="Celebrity Deathmatch"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/4313" className=""><img src="https://i.imgur.com/81obnYi.png" alt="Full House" title="Tanner Family"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/1430" className=""><img src="https://i.imgur.com/uAdlbyf.png" alt="Cosmos" title="Carl Sagan"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/456" className=""><img src="https://i.imgur.com/Z0J4JVS.png" alt="The Simpsons" title="Springfield"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/52" className=""><img src="https://i.imgur.com/fylNpug.png" alt="That '70s Show" title="70s"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/63161" className=""><img src="https://i.imgur.com/HM0JYAF.png" alt="Crazy Ex-Girlfriend" title="Rachel Bloom"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/61222" className=""><img src="https://i.imgur.com/0uB0ib2.png" alt="Bojack Horseman" title="Bojack"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/251" className=""><img src="https://i.imgur.com/LIorqqJ.png" alt="Aqua Teen Hunger Force" title="Meatwad, Shake, and Fry"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/60839" className=""><img src="https://i.imgur.com/aE6SYNl.png" alt="Broad City" title="Abby and Ilana"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/62560" className=""><img src="https://i.imgur.com/im2kayJ.png" alt="Mr. Robot" title="Hacker Man"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/2316" className=""><img src="https://i.imgur.com/JmqJCiW.png" alt="The Office" title="Prison Mike"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/12971" className=""><img src="https://i.imgur.com/4Tx8mGx.png" alt="Dragon Ball Z" title="Dragon Ball Z"/></Link></li>
+                <li className="show-li-top"><Link to="/shows/60573" className=""><img src="https://i.imgur.com/djMlQI5.png" alt="Silicon Valley" title="Pied Piper"/></Link></li>
 
 
             </ul>

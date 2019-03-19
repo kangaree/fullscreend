a22377e92a6757122c906020595aaedac9cb6fb8
Gary Kang <garyzkang@gmail.com>
2019-03-19 14:57:56 -0400

    Change Show Poster to Sticky. Add TV Logos with White Border

--- frontend/components/shows_show/shows_show.jsx
+++ frontend/components/shows_show/shows_show.jsx
@@ -46,111 +46,138 @@ class ShowsShow extends Component {
 
         return (
             <>
-                <div className="backdrop" style={ show.backdrop_path ?
+                <div className="show-backdrop" style={ show.backdrop_path ?
                         { backgroundImage: `url(${"https://image.tmdb.org/t/p/original" + show.backdrop_path})` }
                         : {}}>
                         
-                    <div className="backdrop-fade">
-
-                        <div className="headline-overview">
-
-                            <img className="dashboard-poster"
-                                src={
-                                    show.poster_path ?
-                                        "https://image.tmdb.org/t/p/w500/" + show.poster_path
-                                        : window.testPoster} />
-                                            
-                            <div className="dashboard-middle-text">
-                                <h2 className="headline-show">{show.name}</h2>
-                                <div className="show-info-overview">
-                                    <i className="fas fa-calendar-day"></i>
-                                    <p>{show.first_air_date} to {show.last_air_date} ({show.status})</p>
-                                    <i className="fas fa-tv"></i>
-                                    <p>{show.type}</p>
-                                    <i className="far fa-clock"></i>
-                                    <p>{show.episode_run_time ? show.episode_run_time[0] : null} min</p>
-                                    <i className="fas fa-cloud-sun"></i>
-                                    <p>{show.number_of_seasons} Season{show.number_of_seasons == 1 ? "" : "s"}, {show.number_of_episodes} Episode{show.number_of_episodes == 1 ? "" : "s"}</p>
-                                </div>
-                                <div className="show-overview">{show.overview}</div>
-                            </div>
-
-
-                            {currentUser ? 
-                                <div className="dashboard-buttons">
-                                    <div className="gray-quick-review">
-                                        <a className="gray-quick-review-link">
-                                            <i className="far fa-bookmark" style={currentUserReview ? { color: "#00e24b" } : null}></i>
-                                        </a>
-                                        <a className="gray-quick-review-link">
-                                            <i className="far fa-heart" style={currentUserReview ? (currentUserReview.like ? { color: "orange" } : null) : null}></i>
-                                        </a>
-                                        <a className="gray-quick-review-link">
-                                            <i className="far fa-clock" onClick={() => openModal({ modal_type: 'list' })} show={show}></i>
-                                        </a>
-                                    </div>
-                                    <div className="gray-quick-review">
-                                        <a className="gray-quick-review-star">
-                                            <i className="fas fa-star" style={currentUserReviewScore >= 1 ? {color: "#00e24b"} : null}></i>
-                                        </a>
-                                        <a className="gray-quick-review-star">
-                                            <i className="fas fa-star" style={currentUserReviewScore >= 2 ? { color: "#00e24b" } : null}></i>
-                                        </a>
-                                        <a className="gray-quick-review-star">
-                                            <i className="fas fa-star" style={currentUserReviewScore >= 3 ? { color: "#00e24b" } : null}></i>
-                                        </a>
-                                        <a className="gray-quick-review-star">
-                                            <i className="fas fa-star" style={currentUserReviewScore >= 4 ? { color: "#00e24b" } : null}></i>
-                                        </a>
-                                        <a className="gray-quick-review-star">
-                                            <i className="fas fa-star" style={currentUserReviewScore >= 5 ? { color: "#00e24b" } : null}></i>
-                                        </a>
-                                    </div>
-                                    <a className="gray-button" onClick={() => openModal({ modal_type: 'review' })} show={show}>Review or log...</a>
-                                    <a className="gray-button" onClick={() => openModal({ modal_type: 'list' })} show={show}>Add to a list...</a>
-                                </div>
-                                :
-                                
-                                <div className="dashboard-buttons">
-                                    <a className="gray-button" onClick={() => openModal({ modal_type: 'login' })} show={show}>Sign In</a>
-                                    <a className="gray-button" onClick={() => openModal({ modal_type: 'signup' })} show={show}>Create account to log, rate or review</a>
-                                </div>
-
-                                }
-
-                        </div>
+                    <div className="show-backdrop-fade">
                         
                     </div>
                 
                 </div>
                 
-                <div className="center-horizontally">
-                    <ul className="season-list">
-                        {show.seasons && show.seasons.every(season => season.poster_path) ? show.seasons.map(season => 
-                            <li key={season.name}>
-                                <Link to={"/shows/" + show.id + "/seasons/" + season.season_number}>
-                                    <img src={season.poster_path ?
-                                        "https://image.tmdb.org/t/p/w500" + season.poster_path
-                                        : ""}/>
-                                </Link>
-                            </li> )
+                <img className="dashboard-poster-sticky"
+                    src={
+                        show.poster_path ?
+                            "https://image.tmdb.org/t/p/w500/" + show.poster_path
+                            : window.testPoster} /> 
+                
+                
+                <div className="headline-overview">
+
+                    <div className="dashboard-middle-text">
+                        <h2 className="headline-show">{show.name}</h2>
+                        <div className="headline-show-gray">
+                            {show.genres ?
+                                show.genres.map(
+                                    genre => (<span key={genre.name}>{genre.name} </span>)
+                                )
+                                : null}
+                        </div>
+                        <div className="show-info-overview">
+                            <i className="fas fa-calendar-day"></i>
+                            <p>{show.first_air_date} to {show.last_air_date} ({show.status})</p>
+                            <i className="fas fa-tv"></i>
+                            <p>{show.type}</p>
+                            <i className="far fa-clock"></i>
+                            <p>{show.episode_run_time ? show.episode_run_time[0] : null} min</p>
+                            <i className="fas fa-cloud-sun"></i>
+                            <p>{show.number_of_seasons} Season{show.number_of_seasons == 1 ? "" : "s"}, {show.number_of_episodes} Episode{show.number_of_episodes == 1 ? "" : "s"}</p>
+                        </div>
+                        <div className="show-overview">{show.overview}</div>
+                        <div className="show-networks">
+                            {show.networks ?
+                                show.networks.map(
+                                    network => (<img src={"https://image.tmdb.org/t/p/w300/" + network.logo_path} key={network.name}></img>)
+                                )
+                                : null}
+                        </div>
+                    </div>
+
+
+                    {currentUser ?
+                        <div className="dashboard-buttons">
+                            <div className="gray-quick-review">
+                                <div className="gray-quick-review-link">
+                                    <a className="">
+                                        <i className="far fa-bookmark" style={currentUserReview ? { color: "#00e24b" } : null}></i>
+                                    </a>
+                                    <div>Watch</div>
+                                </div>
+                                <div className="gray-quick-review-link">
+                                    <a className="">
+                                        <i className="far fa-heart" style={currentUserReview ? (currentUserReview.like ? { color: "orange" } : null) : null}></i>
+                                    </a>
+                                    <div>Like</div>
+                                </div>
+                                <div className="gray-quick-review-link">
+                                    <a className="">
+                                        <i className="far fa-clock" onClick={() => openModal({ modal_type: 'list' })} show={show}></i>
+                                    </a>
+                                    <div>List</div>
+                                </div>
+                            </div>
+                            <div className="gray-quick-review">
+                                <div>
+                                    <a className="gray-quick-review-star">
+                                        <i className="fas fa-star" style={currentUserReviewScore >= 1 ? { color: "#00e24b" } : null}></i>
+                                    </a>
+                                    <a className="gray-quick-review-star">
+                                        <i className="fas fa-star" style={currentUserReviewScore >= 2 ? { color: "#00e24b" } : null}></i>
+                                    </a>
+                                    <a className="gray-quick-review-star">
+                                        <i className="fas fa-star" style={currentUserReviewScore >= 3 ? { color: "#00e24b" } : null}></i>
+                                    </a>
+                                    <a className="gray-quick-review-star">
+                                        <i className="fas fa-star" style={currentUserReviewScore >= 4 ? { color: "#00e24b" } : null}></i>
+                                    </a>
+                                    <a className="gray-quick-review-star">
+                                        <i className="fas fa-star" style={currentUserReviewScore >= 5 ? { color: "#00e24b" } : null}></i>
+                                    </a>
+                                    <div className="gray-quick-review-star-caption">Rate</div>
+                                </div>
+                            </div>
+                            <a className="gray-button" onClick={() => openModal({ modal_type: 'review' })} show={show}>Review or log...</a>
+                            <a className="gray-button" onClick={() => openModal({ modal_type: 'list' })} show={show}>Add to a list...</a>
+                        </div>
                         :
-                        null}
-                    </ul>
+                        <div className="dashboard-buttons">
+                            <a className="gray-button" onClick={() => openModal({ modal_type: 'login' })} show={show}>Sign In</a>
+                            <a className="gray-button" onClick={() => openModal({ modal_type: 'signup' })} show={show}>Create account to log, rate or review</a>
+                        </div>
+
+                    }
+
                 </div>
 
-                {currentUser ? reviews.map(review => (
-                    <ReviewListItemContainer
-                        review={review}
-                        key={review.id}
-                        deleteReview={this.props.deleteReview}
-                        updateReview={this.props.updateReview}
-                        openModal={this.props.openModal}
-                        show={show}
-                    />
-                    ))
-                    : 
-                    null}
+                <div className="center-horizontally">
+                    <div className="left-indent-poster">
+                        <ul className="season-list">
+                            {show.seasons ? show.seasons.map(season =>
+                                <li key={season.name}>
+                                    <Link to={"/shows/" + show.id + "/seasons/" + season.season_number}>
+                                        <img src={season.poster_path ?
+                                            "https://image.tmdb.org/t/p/w500" + season.poster_path
+                                            : window.testPoster} />
+                                    </Link>
+                                </li>)
+                                :
+                                null}
+                        </ul>
+                        {currentUser ? reviews.map(review => (
+                            <ReviewListItemContainer
+                                review={review}
+                                key={review.id}
+                                deleteReview={this.props.deleteReview}
+                                updateReview={this.props.updateReview}
+                                openModal={this.props.openModal}
+                                show={show}
+                            />
+                        ))
+                            :
+                            null}
+                    </div>
+                </div>
 
                 {currentUser ? <div className="center-buttons"><a className="green-button-log" onClick={() => openModal({ modal_type: 'review' })} show={show}>log</a></div> : null}
 
